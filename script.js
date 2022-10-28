06ac46f859976defacadfa22c9b4351c0f7e3ffb
Josediaz777 <manueltiscareno1200gmail.com>
2022-10-28 09:40:54 -0500

    Segundo Avance

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let disabled = false;
 
 function cardFlipped({ target: cardSelected }) {
     if (first !== cardSelected && !disabled) {
-        cardSelected.classList.add("flip");
+        cardSelected.classList.add("vuelta");
         if (!first) {
             return first = cardSelected;
         }
@@ -32,13 +32,19 @@ function cardsMatched(img1, img2) {
         }
         first.removeEventListener("click", cardFlipped);
         second.removeEventListener("click", cardFlipped);
+        first.classList.add("par");
+        second.classList.add("par");
         first = second = "";
         return disabled = false;
     }
+    setTimeout(() => {
+        first.classList.add("impar");
+        second.classList.add("impar");
+    }, 200);
 
     setTimeout(() => {
-        first.classList.remove("flip");
-        second.classList.remove("flip");
+        first.classList.remove("vuelta", "impar");
+        second.classList.remove("vuelta", "impar");
         first = second = "";
         disabled = false;
     }, 1200);
@@ -48,10 +54,10 @@ function cardRandom() {
     matched = 0;
     disabled = false;
     first = second = "";
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 1, 2, 3, 4, 5, 6, 7, 8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32];
     arr.sort(() => Math.random() > 0.5 ? 1 : -1);
     cards.forEach((card, i) => {
-        card.classList.remove("flip");
+        card.classList.remove("vuelta");
         let imgTag = card.querySelector(".trasera img");
         imgTag.src = `imagenes/juego-${arr[i]}.png`;
         card.addEventListener("click", cardFlipped);
